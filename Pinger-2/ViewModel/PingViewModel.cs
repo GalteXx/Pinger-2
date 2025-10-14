6853ebd9aa687876f306b8735303f44753c2d795
GalteXx <maximkors@gmail.com>
2025-10-15 01:19:41 +0200

    Partially moved logic from Pinger VM to model WIP to simplify further additional features

--- Pinger-2/ViewModel/PingViewModel.cs
+++ Pinger-2/ViewModel/PingViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Pinger_2.Model;
 using Pinger_2.Service;
 using System.Net;
 
@@ -6,19 +7,14 @@ namespace Pinger_2.ViewModel
 {
     public partial class PingViewModel : ObservableObject
     {
-        // I kind of merged the ViewModel and the Model into one class here, since the Model is so simple
-        public PingViewModel(string _name, string _addressOrDomain, IPAddress address)
+        //well, I was wrong
+        PingTargetModel _model;
+        public PingViewModel(AddressConfig targetConfig)
         {
-            name = _name;
-            domainOrAddress = _addressOrDomain;
+            _model = new PingTargetModel(targetConfig);
 
-            _pingService = new ICMPPinger(address);
-            _pingService.PingReceived += (sender, e) =>
-            {
-                Ping = e;
-                TimeSinceLastRequest = TimeSpan.Zero;
-                UpdateTimer();
-            };
+            Name = _model.Name;
+            DomainOrAddress = _model.AddressOrDomain;
         }
 
 
@@ -30,22 +26,5 @@ namespace Pinger_2.ViewModel
         private TimeSpan ping;
         [ObservableProperty]
         private TimeSpan timeSinceLastRequest;
-
-        private IPingService _pingService;
-
-        public void StartPinging()
-        {
-            _pingService.Start();
-        }
-        public void StopPinging()
-        {
-            _pingService.Stop();
-        }
-
-        public void UpdateTimer() //so long it works
-        {
-            TimeSinceLastRequest = _pingService.PingWaitingSpan;
-        }
-
     }
 }
